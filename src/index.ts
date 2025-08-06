d23be3a6329dab2e932a028c0cf3eb2fa37e4989
JosePuelloCortina <puelloj66@gmail.com>
2025-08-06 09:24:33 -0500

    eliminando archivo dist

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 async function main() {
     try {
         await AppDataSource.initialize();
+        console.log("Listening on:", process.env.PORT);
         app.listen(PORT);
         console.log(`Server running on port ${PORT}`);
     } catch (error) {
