9760bce01e8cf66d31ad1567b0caa2ce49005144
JosePuelloCortina <puelloj66@gmail.com>
2025-08-08 08:03:50 -0500

    feat: creando controlador para obtener poryecto por id

--- src/routes/realstate/RealStateRoutes.ts
+++ src/routes/realstate/RealStateRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 
 import { allRealState } from "../../controllers/realStateController/AllRealState";
 import { createRealState } from "../../controllers/realStateController/CreateRealState";
+import { getRealStateById } from "../../controllers/realStateController/GetRealStateById";
 
 
 const router = Router()
 
 router.get("/all-projects", allRealState);
 router.post("/create-project", createRealState);
+router.get("/get-project-by-id/:id", getRealStateById);
 
 export default router;
